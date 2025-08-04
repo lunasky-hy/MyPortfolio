612ccdc288b86077b1140167c92c033b863b61af
山田啓允 <lunasky@HiroyMacBookAir.local>
2025-08-04 17:36:26 +0900

    create title content

--- index.tsx
+++ index.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '/src/stylesheets/index.css'
 import PortfolioMain from './src/pages/PortfolioMain'
 
 createRoot(document.getElementById('root')!).render(
--- src/pages/PortfolioMain.tsx
+++ src/pages/PortfolioMain.tsx
@@ -1,5 +1,38 @@
-
+import React from "react";
 
 export default function PortfolioMain() {
-  return <div>Hello! This is new world!</div>
+  const [displayedTitleLen, setTitleLen] = React.useState(0);
+  const timerRef = React.useRef(0);
+
+  const titleString = 'console.log(getPortfolio("hiroy"));';
+
+  React.useEffect(() => {
+    if (displayedTitleLen < titleString.length) {
+      timerRef.current = setTimeout(
+        () => setTitleLen(displayedTitleLen + 1),
+        200 + (Math.random() * 100 - 150)
+      );
+    }
+    return () => clearTimeout(timerRef.current);
+  }, [displayedTitleLen]);
+
+
+  return (<>
+      <div id="index-title" className="full-height-content">
+        <h1 id="title-console">{titleString.slice(0, displayedTitleLen)}</h1>
+        <div className={displayedTitleLen < titleString.length ? "hidden" : "title-fadein"}>
+          <h1 id="title-response">&gt; &nbsp; ポートフォリオを表示</h1> 
+          <ul id="index-list">
+            <li>Readme</li>
+            <li>ライフログ（経歴）</li>
+            <li>プロダクト</li>
+            <li>テクニカルグラフ（スキルセット）</li>
+            <li>趣味</li>
+          </ul>
+        </div>
+      </div>
+      <div id="aboutme" className="full-height-content">
+        <h1>About Me</h1>
+      </div>
+    </>);
 }
